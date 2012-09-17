9dde08935cd87ece8cd5b2cf4ac0d3e034765998
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2012-09-18 00:17:07 +0300

    Handle resources, enable caching for resources

--- src/main/java/com/codimage/config/WebConfig.java
+++ src/main/java/com/codimage/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -20,10 +21,22 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan("com.codimage.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    /** The cache period for static resources (js, css, static images) */
+    private static final int STATIC_CACHE_PERIOD = 1209600; // 14 days
+
     @Override public void addViewControllers(ViewControllerRegistry registry) {
         super.addViewControllers(registry);
 
-        registry.addViewController("/index.html");
+        registry.addViewController("/404");
+        registry.addViewController("/500");
+    }
+
+    @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(STATIC_CACHE_PERIOD);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(STATIC_CACHE_PERIOD);
+        registry.addResourceHandler("/image/**").addResourceLocations("/image/").setCachePeriod(STATIC_CACHE_PERIOD);
     }
 
     @Bean public ViewResolver viewResolver() {
